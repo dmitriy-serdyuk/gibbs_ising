a65aad8c95347fc4b877b5f02cfd423a32d9a8ff
serdyuk <serdyuk.dmitriy@gmail.com>
2014-11-24 11:56:11 -0500

    Added logic

--- main.py
+++ main.py
@@ -1,29 +1,52 @@
 import argparse
 import numpy as np
-import scipy.io
-
-
-def read_data(filename):
-    data = scipy.io.loadmat(filename)
-    data = np.array(data['price_move'])[:, 0]
-    n = len(data)
-    return n, data
+from numpy.random import binomial
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
         "Run Gibbs sampling on Ising model")
-    parser.add_argument("--input",
-                        type=str, help="Input .mat file", default='file.mat')
-    parser.add_argument("--q-value",
-                        type=float, default=0.7,
-                        help="Value of q")
-    parser.add_argument("--prob-x1",
-                        type=float, default=0.2,
-                        help="Probability of x1")
+    parser.add_argument("--iter",
+                        type=int, default=1000,
+                        help="Number of iterations")
+    parser.add_argument("--size",
+                        type=int, default=30,
+                        help="Size of Ising model")
+    parser.add_argument("--theta",
+                        type=float, default=0.45,
+                        help="Coupling parameter")
+    parser.add_argument("--show-freq",
+                        type=int, default=100,
+                        help="Show frequency")
     return parser.parse_args()
+
+
+def main(num_iter, size, theta, show_freq):
+    rng = np.random.RandomState(123)
+    vars = (rng.binomial(1, 0.5, size=(size, size)) * 2) - 1
+    ans = []
+    for it in xrange(num_iter):
+        for i in xrange(size):
+            for j in xrange(size):
+                sum = 0
+                if i != 0:
+                    sum += theta * vars[i - 1, j]
+                if i != size - 1:
+                    sum += theta * vars[i + 1, j]
+                if j != 0:
+                    sum += theta * vars[i, j - 1]
+                if j != size - 1:
+                    sum += theta * vars[i, j + 1]
+                prob_neg = np.exp(-sum)
+                prob_pos = np.exp(sum)
+                sample = rng.binomial(1, prob_pos / (prob_neg + prob_pos))
+                vars[i, j] = (sample * 2) - 1
+        if it % show_freq == 0:
+            ans += [vars.copy()]
+
+    return ans
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args.input, args.q_value, args.prob_x1)
+    main(args.iter, args.size, args.theta, args.show_freq)
 
